09d748304082f7246dd0d8c7fa1b7dfb3115e47f
gabcarrara <gabcarrara@hotmail.com>
2016-06-26 21:20:21 -0300

    Mais métodos na sessão.

--- trabalho-dw-1/src/gerenciamento/Sessao.java
+++ trabalho-dw-1/src/gerenciamento/Sessao.java
@@ -5,6 +5,7 @@
  */
 package gerenciamento;
 
+import java.time.LocalDate;
 import java.util.List;
 import modelo.*;
 
@@ -16,42 +17,77 @@ public class Sessao {
 
     private Cliente client;
     private CarrinhoDeCompra kart;
+    private Compra compraAtual;
     private GerenciarCliente gerenteCliente;
     private GerenciarEventos gerenteEvento;
+    private boolean logado = false;
+
+    public Sessao() {
+        this.kart = new CarrinhoDeCompra();
+        this.compraAtual = new Compra();
+        this.gerenteCliente = new GerenciarCliente();
+        this.gerenteEvento = new GerenciarEventos();
+    }
+
+    public boolean isLogado() {
+        return logado;
+    }
 
     public boolean fazerLogin(String usuario, String senha) {
         client = gerenteCliente.getCliente(usuario);
-        return client != null && client.getSenha().equals(senha);
+        if (client != null && client.getSenha().equals(senha)) {
+            logado = true;
+        }
+        return logado;
     }
 
     public void exibirHistorico() {
-        List<Compra> compras = client.getHistorico().getHistoricoDeCompras();
-        for (Compra c : compras) {
-            System.out.println(c.toString());
+        if (logado) {
+            List<Compra> compras = client.getHistorico().getHistoricoDeCompras();
+            for (Compra c : compras) {
+                System.out.println(c.toString());
+            }
+        } else {
+            System.out.println("Por favor faça o seu login.");
         }
     }
 
     public void exibirCatalogo() {
-        List<Evento> eventos = gerenteEvento.gerarCatalogo();
-        System.out.println("Eventos disponíveis:");
-        for (Evento e : eventos) {
-            e.toString();
-            System.out.println("");
+        if (logado) {
+            List<Evento> eventos = gerenteEvento.gerarCatalogo();
+            System.out.println("Eventos disponíveis:");
+            for (Evento e : eventos) {
+                e.toString();
+                System.out.println("");
+            }
+        } else {
+            System.out.println("Por favor faça o seu login.");
         }
 
     }
-    
-    public boolean adcionarNoCarrinho(int eventoID, int qtd){
-        if (qtd < 4) {
+
+    public boolean adcionarNoCarrinho(int eventoID, int qtd) {
+        if (qtd < 1 || qtd > 4) {
             return false;
         }
-        
-        for (int i = 0; i < qtd-1; i++) {
+
+        for (int i = 0; i < qtd - 1; i++) {
             kart.adicionar(gerenteEvento.getIngresso(eventoID));
         }
-        
+
         return true;
     }
-    
-    
+
+    public void finalizarCompra() {
+        compraAtual.setCarrinho(kart);
+        compraAtual.setDataDaCompra(LocalDate.now().toString());
+        compraAtual.setId((int) (Math.random() * 1000));
+
+    }
+
+    public void efetuarPagamento() {
+        client.getHistorico().adiciona(compraAtual);
+        compraAtual = new Compra();
+        kart = new CarrinhoDeCompra();
+    }
 }
