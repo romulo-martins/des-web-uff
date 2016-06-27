f1dd36e0a4b242b0244f0e7de789cf5a9d088848
gabcarrara <gabcarrara@hotmail.com>
2016-06-26 21:49:47 -0300

    Adição de passos na main.

--- trabalho-dw-1/src/execucao/Main.java
+++ trabalho-dw-1/src/execucao/Main.java
@@ -8,6 +8,7 @@ package execucao;
 import dados.Seed;
 import gerenciamento.GerenciarCliente;
 import gerenciamento.GerenciarEventos;
+import gerenciamento.Sessao;
 import modelo.Cliente;
 
 /**
@@ -15,7 +16,7 @@ import modelo.Cliente;
  * @author Romulo
  */
 public class Main {
-    
+
     public static void main(String[] args) {
 
         // responsavel pelas operações de eventos
@@ -25,18 +26,35 @@ public class Main {
         // exibe os eventos
         System.out.println("----- Eventos -----");
         ge.listarEventos();
-        
+
         GerenciarCliente gc = new GerenciarCliente();
         // popula com os dados de exemplo
         gc.seedClientes(Seed.getClientes());
-        
+
         // suponha que o cliente 3 (Flavia Almeida) quer comprar ingressos
         Cliente c = gc.getCliente(3);
         System.out.println("----- Cliente selecionado -----");
         System.out.println(c);
+
+        //Flavia faz login para iniciar sessão
+        Sessao se = new Sessao();
+        se.fazerLogin(c.getNome(), c.getSenha());
+
+        // Flavia escolhe uma das opções de evento e adciona no carrinho de compras
+        System.out.println("Catálogo");
+        se.exibirCatalogo();
+        se.adcionarNoCarrinho(4, 1);
+
+        //Flavia confirma a compra e efetua o pagamento
+        se.finalizarCompra();
+        se.efetuarPagamento();
         
-        // Flavia escolhe uma das opções
+        //Flavia verifica o histórico para ver sa compra foi feita
+        System.out.println("Histórico");
+        se.exibirHistorico();
         
+        
+        //verificar se o ingresso foi retirado do estoque do evento
         
     }
 }
