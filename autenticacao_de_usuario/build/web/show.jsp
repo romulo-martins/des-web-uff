17ea6f43405128c61e8b2b3217eaf5441d078b39
Romulo <romuluxs@gmail.com>
2016-07-03 19:40:20 -0300

    Exercitando sessoes e autenticação

---
+++ autenticacao_de_usuario/build/web/show.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : show
+    Created on : 03/07/2016, 15:30:50
+    Author     : Romulo
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Show</title>
+    </head>
+    <body>
+        <h1>Show result!</h1>
+        Thanks <em> ${name} </em> for passing by. <br/>
+        The total is: ${total}. <br/>
+
+        <br/>
+        ${warning}
+    </body>
+</html>
---
+++ autenticacao_de_usuario/src/java/factory/ConnectionFactory.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Romulo
+ */
+public class ConnectionFactory {
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:derby://localhost/Servlet", "Servlet", "root");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
